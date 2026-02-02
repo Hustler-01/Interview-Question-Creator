3e9e32c91b106c62ab09648f271c469989881534
Azmeen <azmeenkhatoon2704@gmail.com>
2026-02-02 20:45:53 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Interview Question Creator",
+    version= '0.0.0',
+    author="Azmeen Khatoon",
+    author_email="azmeenkhatoon2704@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
